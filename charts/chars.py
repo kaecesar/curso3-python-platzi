18eb2d4c7cc8449ac0fc5b4168ea65902ad18bc0
Santiago Cesar <santivancesar@gmail.com>
2023-01-12 15:10:29 -0300

    agregue generador de pie chart

---
+++ charts/chars.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+    labels = ['a', 'v', 'c']
+    values = [200, 100, 30]
+
+    fig, ax = plt.subplots()
+    ax.pie(values, labels = labels)
+    plt.savefig('pie.png')
+    plt.close
+
+
---
+++ charts/main.py
@@ -0,0 +1,7 @@
+import chars
+
+def run():
+    chars.generate_pie_chart()
+
+if __name__ == "__main__":
+    run()
